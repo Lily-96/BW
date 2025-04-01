ed1d6c3482f84eeeabe30a0874fac920ab41c7b4
IC Tech <148203290+ivancroce@users.noreply.github.com>
2025-04-01 16:51:28 +0100

    to continue
    questions completed,
    need to link the results page,
    fix the timer,
    CSS

--- assets/script-quiz.js
+++ assets/script-quiz.js
@@ -5,7 +5,7 @@ const questions = [
     difficulty: "easy",
     question: "What does CPU stand for?",
     correct_answer: "Central Processing Unit",
-    incorrect_answers: ["Central Process Unit", "Computer Personal Unit", "Central Processor Unit"],
+    incorrect_answers: ["Central Process Unit", "Computer Personal Unit", "Central Processor Unit"]
   },
   {
     category: "Science: Computers",
@@ -13,7 +13,7 @@ const questions = [
     difficulty: "easy",
     question: "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn't get modified?",
     correct_answer: "Final",
-    incorrect_answers: ["Static", "Private", "Public"],
+    incorrect_answers: ["Static", "Private", "Public"]
   },
   {
     category: "Science: Computers",
@@ -21,7 +21,7 @@ const questions = [
     difficulty: "easy",
     question: "The logo for Snapchat is a Bell.",
     correct_answer: "False",
-    incorrect_answers: ["True"],
+    incorrect_answers: "True"
   },
   {
     category: "Science: Computers",
@@ -29,7 +29,7 @@ const questions = [
     difficulty: "easy",
     question: "Pointers were not used in the original C programming language; they were added later on in C++.",
     correct_answer: "False",
-    incorrect_answers: ["True"],
+    incorrect_answers: "True"
   },
   {
     category: "Science: Computers",
@@ -37,7 +37,7 @@ const questions = [
     difficulty: "easy",
     question: "What is the most preferred image format used for logos in the Wikimedia database?",
     correct_answer: ".svg",
-    incorrect_answers: [".png", ".jpeg", ".gif"],
+    incorrect_answers: [".png", ".jpeg", ".gif"]
   },
   {
     category: "Science: Computers",
@@ -45,7 +45,7 @@ const questions = [
     difficulty: "easy",
     question: "In web design, what does CSS stand for?",
     correct_answer: "Cascading Style Sheet",
-    incorrect_answers: ["Counter Strike: Source", "Corrective Style Sheet", "Computer Style Sheet"],
+    incorrect_answers: ["Counter Strike: Source", "Corrective Style Sheet", "Computer Style Sheet"]
   },
   {
     category: "Science: Computers",
@@ -53,7 +53,7 @@ const questions = [
     difficulty: "easy",
     question: "What is the code name for the mobile operating system Android 7.0?",
     correct_answer: "Nougat",
-    incorrect_answers: ["Ice Cream Sandwich", "Jelly Bean", "Marshmallow"],
+    incorrect_answers: ["Ice Cream Sandwich", "Jelly Bean", "Marshmallow"]
   },
   {
     category: "Science: Computers",
@@ -61,7 +61,7 @@ const questions = [
     difficulty: "easy",
     question: "On Twitter, what is the character limit for a Tweet?",
     correct_answer: "140",
-    incorrect_answers: ["120", "160", "100"],
+    incorrect_answers: ["120", "160", "100"]
   },
   {
     category: "Science: Computers",
@@ -69,7 +69,7 @@ const questions = [
     difficulty: "easy",
     question: "Linux was first created as an alternative to Windows XP.",
     correct_answer: "False",
-    incorrect_answers: ["True"],
+    incorrect_answers: "True"
   },
   {
     category: "Science: Computers",
@@ -77,8 +77,8 @@ const questions = [
     difficulty: "easy",
     question: "Which programming language shares its name with an island in Indonesia?",
     correct_answer: "Java",
-    incorrect_answers: ["Python", "C", "Jakarta"],
-  },
+    incorrect_answers: ["Python", "C", "Jakarta"]
+  }
 ];
 
 const questionContainer = document.getElementById(`questions-container`);
@@ -104,14 +104,20 @@ const allQuestions = () => {
 
   notBoldTitle.innerText = question.question;
 
+  btn1.style.display = "inline-block";
+  btn2.style.display = "inline-block";
+  btn3.style.display = "inline-block";
+  btn4.style.display = "inline-block";
+
   let shuffledMultipleAnswers = [...question.incorrect_answers, question.correct_answer].sort(() => Math.random() - 0.5);
-  let shuffledBooleanAnswers = ["true", "false"].sort(() => Math.random() - 0.5);
 
   if (question.type === "multiple") {
     buttons.forEach((btn, index) => {
       btn.innerText = shuffledMultipleAnswers[index];
+      btn.classList.add(`hover`);
 
       btn.onclick = () => {
+        btn.classList.remove(`hover`);
         if (btn.innerText === question.correct_answer) {
           generalScore++;
           btn.classList.add("correct-answer");
@@ -130,10 +136,18 @@ const allQuestions = () => {
       };
     });
   } else if (question.type === "boolean") {
-    buttons.forEach((btn, index) => {
-      btn.innerText = shuffledBooleanAnswers[index];
+    buttons.forEach((btn) => {
+      btn.classList.add(`hover`);
+      if (question.correct_answer === `True`) {
+        btn1.innerText = question.correct_answer;
+        btn2.innerText = question.incorrect_answers;
+      } else if (question.incorrect_answers === `True`) {
+        btn1.innerText = question.incorrect_answers;
+        btn2.innerText = question.correct_answer;
+      }
 
       btn.onclick = () => {
+        btn.classList.remove(`hover`);
         if (btn.innerText === question.correct_answer) {
           generalScore++;
           btn.classList.add("correct-answer");
@@ -152,11 +166,25 @@ const allQuestions = () => {
       };
     });
 
-    btn1.style.margin = "60px";
-    btn2.style.margin = "60px";
     btn3.style.display = "none";
     btn4.style.display = "none";
   }
 };
+
+let timeLeft = 40;
+const totalTime = 16;
+const circle = document.querySelector(".circle");
+const timerText = document.getElementById("timer");
+
+function updateTimer() {
+  timeLeft--;
+  const progress = (timeLeft / totalTime) * 314;
+  circle.style.strokeDashoffset = progress;
+  timerText.textContent = timeLeft;
+  if (timeLeft > 0) {
+    setTimeout(updateTimer, 1000);
+  }
+}
+setTimeout(updateTimer, 1000);
 
 allQuestions();
