128259df4de92379daa033c643cd62c0a913ede6
IC Tech <148203290+ivancroce@users.noreply.github.com>
2025-04-01 09:40:02 +0100

    welcome page completed
    waiting to link the next page once it's ready

---
+++ assets/welcome-page-script.js
@@ -0,0 +1,20 @@
+const proceed = () => {
+  const checkBox = document.getElementById(`checkbox`);
+  const button = document.querySelector(`button`);
+
+  checkBox.addEventListener(`change`, function () {
+    if (!checkBox.checked) {
+      button.classList.remove(`enabled`);
+      button.disabled = true;
+      button.style.cursor = `not-allowed`;
+    } else {
+      button.classList.add(`enabled`);
+      button.disabled = false;
+      button.style.cursor = `pointer`;
+      /* button.onclick = () => {
+      window.location.href = `quiz.html`;
+      }; */
+    }
+  });
+};
+proceed();
