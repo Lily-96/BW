4369e94ef42e277fdfbe051f80db4fb88762dc64
roccadavide <davide.rocca03@gmail.com>
2025-04-01 15:39:00 +0200

    js mess

--- assets/script-quiz.js
+++ assets/script-quiz.js
@@ -3,8 +3,7 @@ const questions = [
     category: "Science: Computers",
     type: "multiple",
     difficulty: "easy",
-    question_not_bold: "What does CPU",
-    question_bold: "stand for?",
+    question: "What does CPU stand for?",
     correct_answer: "Central Processing Unit",
     incorrect_answers: ["Central Process Unit", "Computer Personal Unit", "Central Processor Unit"],
   },
@@ -12,8 +11,7 @@ const questions = [
     category: "Science: Computers",
     type: "multiple",
     difficulty: "easy",
-    question_not_bold: "In the programming language Java, which of these keywords would you",
-    question_bold: "put on a variable to make sure it doesn&#039;t get modified?",
+    question: "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn't get modified?",
     correct_answer: "Final",
     incorrect_answers: ["Static", "Private", "Public"],
   },
@@ -21,8 +19,7 @@ const questions = [
     category: "Science: Computers",
     type: "boolean",
     difficulty: "easy",
-    question_not_bold: "The logo for Snapchat",
-    question_bold: "is a Bell.",
+    question: "The logo for Snapchat is a Bell.",
     correct_answer: "False",
     incorrect_answers: ["True"],
   },
@@ -30,8 +27,7 @@ const questions = [
     category: "Science: Computers",
     type: "boolean",
     difficulty: "easy",
-    question_not_bold: "Pointers were not used in the original C programming language; they were added later on in C++.",
-    question_bold: "they were added later on in C++.",
+    question: "Pointers were not used in the original C programming language; they were added later on in C++.",
     correct_answer: "False",
     incorrect_answers: ["True"],
   },
@@ -39,8 +35,7 @@ const questions = [
     category: "Science: Computers",
     type: "multiple",
     difficulty: "easy",
-    question_not_bold: "What is the most preferred image format",
-    question_bold: "used for logos in the Wikimedia database?",
+    question: "What is the most preferred image format used for logos in the Wikimedia database?",
     correct_answer: ".svg",
     incorrect_answers: [".png", ".jpeg", ".gif"],
   },
@@ -48,8 +43,7 @@ const questions = [
     category: "Science: Computers",
     type: "multiple",
     difficulty: "easy",
-    question_not_bold: "In web design, what does",
-    question_bold: "CSS stand for?",
+    question: "In web design, what does CSS stand for?",
     correct_answer: "Cascading Style Sheet",
     incorrect_answers: ["Counter Strike: Source", "Corrective Style Sheet", "Computer Style Sheet"],
   },
@@ -57,8 +51,7 @@ const questions = [
     category: "Science: Computers",
     type: "multiple",
     difficulty: "easy",
-    question_not_bold: "What is the code name for the mobile",
-    question_bold: "operating system Android 7.0?",
+    question: "What is the code name for the mobile operating system Android 7.0?",
     correct_answer: "Nougat",
     incorrect_answers: ["Ice Cream Sandwich", "Jelly Bean", "Marshmallow"],
   },
@@ -66,8 +59,7 @@ const questions = [
     category: "Science: Computers",
     type: "multiple",
     difficulty: "easy",
-    question_not_bold: "On Twitter, what is the character",
-    question_bold: "limit for a Tweet?",
+    question: "On Twitter, what is the character limit for a Tweet?",
     correct_answer: "140",
     incorrect_answers: ["120", "160", "100"],
   },
@@ -75,8 +67,7 @@ const questions = [
     category: "Science: Computers",
     type: "boolean",
     difficulty: "easy",
-    question_not_bold: "Linux was first created as an",
-    question_bold: "alternative to Windows XP.",
+    question: "Linux was first created as an alternative to Windows XP.",
     correct_answer: "False",
     incorrect_answers: ["True"],
   },
@@ -84,8 +75,7 @@ const questions = [
     category: "Science: Computers",
     type: "multiple",
     difficulty: "easy",
-    question_not_bold: "Which programming language shares its name",
-    question_bold: "with an island in Indonesia?",
+    question: "Which programming language shares its name with an island in Indonesia?",
     correct_answer: "Java",
     incorrect_answers: ["Python", "C", "Jakarta"],
   },
@@ -98,7 +88,7 @@ const btn2 = document.querySelector(`.btn2`);
 const btn3 = document.querySelector(`.btn3`);
 const btn4 = document.querySelector(`.btn4`);
 const notBoldTitle = document.querySelector(`h1`);
-const boldTitle = document.getElementById(`bold-text`);
+const spanScore = document.querySelector(`.score`);
 
 let generalScore = 0;
 let questionNumber = 0;
@@ -107,37 +97,66 @@ const buttons = [btn1, btn2, btn3, btn4];
 
 const allQuestions = () => {
   if (questionNumber >= questions.length) {
-    console.log("Quiz completato!");
-    return;
+    // window.location.href = "";
   }
 
   const question = questions[questionNumber];
 
-  notBoldTitle.innerText = question.question_not_bold;
-  boldTitle.innerText = question.question_bold;
+  notBoldTitle.innerText = question.question;
 
-  let shuffledAnswers = [...question.incorrect_answers, question.correct_answer].sort(() => Math.random() - 0.5);
+  let shuffledMultipleAnswers = [...question.incorrect_answers, question.correct_answer].sort(() => Math.random() - 0.5);
+  let shuffledBooleanAnswers = ["true", "false"].sort(() => Math.random() - 0.5);
 
-  buttons.forEach((btn, index) => {
-    btn.innerText = shuffledAnswers[index];
+  if (question.type === "multiple") {
+    buttons.forEach((btn, index) => {
+      btn.innerText = shuffledMultipleAnswers[index];
 
-    btn.onclick = () => {
-      if (btn.innerText === question.correct_answer) {
-        generalScore++;
-        btn.classList.add("correct-answer");
-        console.log("Risposta corretta! Score:", generalScore);
-      } else {
-        btn.classList.add("wrong-answer");
-        console.log("Risposta sbagliata! Score:", generalScore);
-      }
+      btn.onclick = () => {
+        if (btn.innerText === question.correct_answer) {
+          generalScore++;
+          btn.classList.add("correct-answer");
+          console.log("Risposta corretta! Score:", generalScore);
+        } else {
+          btn.classList.add("wrong-answer");
+          console.log("Risposta sbagliata! Score:", generalScore);
+        }
 
-      setTimeout(() => {
-        btn.classList.remove("correct-answer", "wrong-answer");
-        questionNumber++;
-        allQuestions();
-      }, 1000);
-    };
-  });
+        setTimeout(() => {
+          btn.classList.remove("correct-answer", "wrong-answer");
+          questionNumber++;
+          spanScore.innerText = questionNumber + 1;
+          allQuestions();
+        }, 1000);
+      };
+    });
+  } else if (question.type === "boolean") {
+    buttons.forEach((btn, index) => {
+      btn.innerText = shuffledBooleanAnswers[index];
+
+      btn.onclick = () => {
+        if (btn.innerText === question.correct_answer) {
+          generalScore++;
+          btn.classList.add("correct-answer");
+          console.log("Risposta corretta! Score:", generalScore);
+        } else {
+          btn.classList.add("wrong-answer");
+          console.log("Risposta sbagliata! Score:", generalScore);
+        }
+
+        setTimeout(() => {
+          btn.classList.remove("correct-answer", "wrong-answer");
+          questionNumber++;
+          spanScore.innerText = questionNumber + 1;
+          allQuestions();
+        }, 1000);
+      };
+    });
+
+    btn1.style.margin = "60px";
+    btn2.style.margin = "60px";
+    btn3.style.display = "none";
+    btn4.style.display = "none";
+  }
 };
 
 allQuestions();
